46fba97c00f7880d2757b3425fb293d85abe6581
geiss <geiss@minds-medical.de>
2018-12-23 12:20:47 +0100

    SpringBoot Server init

---
+++ freezeServer/src/main/java/de/mrfreeze/freezeServer/FreezeServerApplication.java
@@ -0,0 +1,14 @@
+package de.mrfreeze.freezeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreezeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreezeServerApplication.class, args);
+	}
+
+}
+
---
+++ freezeServer/src/test/java/de/mrfreeze/freezeServer/FreezeServerApplicationTests.java
@@ -0,0 +1,17 @@
+package de.mrfreeze.freezeServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreezeServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

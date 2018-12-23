54756905e273d44da0234574839cc41affa8856a
geiss <geiss@minds-medical.de>
2018-12-23 15:31:38 +0100

    Xcode init

---
+++ freezeIOSApp/MrFreeze/MrFreeze/MyFridgeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyFridgeViewController.swift
+//  MrFreeze
+//
+//  Created by wer auch immer on 23.12.18.
+//  Copyright © 2018 Jacques. All rights reserved.
+//
+
+import UIKit
+
+class MyFridgeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ freezeIOSApp/MrFreeze/MrFreeze/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MrFreeze
+//
+//  Created by wer auch immer on 23.12.18.
+//  Copyright © 2018 Jacques. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
